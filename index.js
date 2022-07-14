d8968e858f29aeb1c08055b9fe16c353ed0a2cc3
Fábio Francisco da Siva <66127617+binhu8@users.noreply.github.com>
2022-07-14 14:23:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fs = require('fs')
+const fs = require('fs');
+const { query } = require('express');
 let port =  process.env.PORT || 3001
 
 app.use((req, res, next) => {
@@ -18,27 +19,31 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res)=> {
 
-    (async (link) => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(link);
-      
-        const srcList = await page.evaluate(()=> {
-          const nodeList = document.querySelectorAll('img');
-          const imageList = [...nodeList];
-
-          imageList.splice(0,1)
-          const srcList = imageList.map(({src}) => ({src}))
+    if(req.query.link){
+        (async (link) => {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(link);
           
-          
-          return srcList
-        }).catch(erro => console.log(erro));
-
-        res.send(srcList)
-        browser.close()
-
-      }) (req.query.link);
-
+            const srcList = await page.evaluate(()=> {
+              const nodeList = document.querySelectorAll('img');
+              const imageList = [...nodeList];
+    
+              imageList.splice(0,1)
+              const srcList = imageList.map(({src}) => ({src}))
+              
+              
+              return srcList
+            }).catch(erro => console.log(erro));
+    
+            res.send(srcList)
+            browser.close()
+    
+          }) (req.query.link);
+    
+    }else{
+        res.send('bem-vindo ao extrator')
+    }
      
 
     
